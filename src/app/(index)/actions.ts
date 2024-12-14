983521f7915f9c4e831f3f4c111a7b37c2adc3cf
Joel Häkkinen <joel.hakkinen@gmail.com>
2024-12-14 15:56:22 +0200

    notifications, create board actions with useActionState

--- src/app/(index)/actions.ts
+++ src/app/(index)/actions.ts
@@ -4,18 +4,32 @@ import { db } from "@/db";
 import { boards } from "@/db/schema";
 import { v4 as uuid } from "uuid";
 import bcrypt from "bcrypt";
+import type { CreateBoardActionState } from "./create-board-drawer";
 
-export const createBoardAction = async (_previousState: string, formData: FormData) => {
+export const createBoardAction = async (_previousState: CreateBoardActionState, formData: FormData): Promise<CreateBoardActionState> => {
   const name = formData.get("name") as string;
   const password = formData.get("password") as string;
-  await delay(2000)
+  await delay(2000);
 
-  bcrypt.hash(password, 10, async (err, hash) => {
-    if (err) throw err;
-    await db.insert(boards).values({ id: uuid(), name, passwordHash: hash });
-  });
-  new Promise(resolve => setTimeout(resolve, 1000));
-  return name;
+  let passwordHash: string;
+  try {
+    passwordHash = await bcrypt.hash(password, 10);
+  } catch (err) {
+    if (err instanceof Error) {
+      return { status: "error", errorType: "HASHING_ERROR", message: err.message };
+    }
+    return { status: "error", errorType: "UNKNOWN_ERROR", message: "Error happened" };
+  }
+
+  try {
+    await db.insert(boards).values({ id: uuid(), name, passwordHash });
+  } catch (err) {
+    if (err instanceof Error)
+      return { status: "error", errorType: "DB_ERROR", message: err.message };
+    return { status: "error", errorType: "UNKNOWN_ERROR", message: "Error happened" };
+  }
+
+  return { status: "success", message: `Board ${name} created!` };
 };
 
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
--- src/app/(index)/create-board-drawer.tsx
+++ src/app/(index)/create-board-drawer.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useActionState } from "react";
+import { useState, useActionState, useRef, type ComponentRef, type ComponentProps, type RefObject, useEffect } from "react";
 
 import {
   Drawer,
@@ -18,13 +18,16 @@ import { Label } from "../../components/ui/label";
 import { Input } from "../../components/ui/input";
 import { cn } from "@/lib/utils";
 import { createBoardAction } from "./actions";
+import { useToast } from "@/hooks/use-toast";
 
 export const CreateBoardDrawer = () => {
   const [open, setOpen] = useState(false);
 
+  const drawerTriggerRef = useRef<ComponentRef<typeof DrawerTrigger>>(null!);
+
   return (
     <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild>
+      <DrawerTrigger asChild ref={drawerTriggerRef}>
         <Button>Create New</Button>
       </DrawerTrigger>
       <DrawerContent>
@@ -34,7 +37,7 @@ export const CreateBoardDrawer = () => {
             Give it a name and share your tasks with others!
           </DrawerDescription>
         </DrawerHeader>
-        <CreateBoardForm className="px-4" />
+        <CreateBoardForm className="px-4" drawerRef={drawerTriggerRef} />
         <DrawerFooter className="pt-2">
           <DrawerClose asChild>
             <Button className="w-36" variant="outline">Cancel</Button>
@@ -45,9 +48,59 @@ export const CreateBoardDrawer = () => {
   );
 };
 
+type InitialState = {
+  status: "initial",
+}
 
-const CreateBoardForm = ({ className }: React.ComponentProps<"form">) => {
-  const [_message, createBoard, pending] = useActionState(createBoardAction, "");
+type ErrorState = {
+  status: "error";
+  errorType: string;
+  message: string;
+};
+
+type SuccessState = {
+  status: "success";
+  message: string;
+};
+
+export type CreateBoardActionState = InitialState | ErrorState | SuccessState;
+
+const initialState = {
+  status: "initial",
+} as const;
+
+interface CreateBoardFormProps extends ComponentProps<"form"> {
+  drawerRef: RefObject<HTMLButtonElement>
+}
+
+const CreateBoardForm = ({
+  className,
+  drawerRef,
+}: CreateBoardFormProps) => {
+  const [state, createBoard, pending] = useActionState(createBoardAction, initialState);
+  const { toast } = useToast();
+
+  useEffect(() => {
+    switch (state.status) {
+      case "initial":
+        return;
+      case "error":
+        toast({
+          variant: "destructive",
+          title: "Error Happened",
+          description: state.message,
+        });
+        return;
+      case "success":
+        toast({
+          title: "Board Created",
+          description: state.message
+        });
+        drawerRef.current.click();
+        return;
+    }
+  }, [state, pending, toast, drawerRef]);
+
   return (
     <form
       action={createBoard}
@@ -55,7 +108,7 @@ const CreateBoardForm = ({ className }: React.ComponentProps<"form">) => {
     >
       <div className="grid gap-2">
         <Label htmlFor="boardName">Board name</Label>
-        <Input name="name" id="boardName" defaultValue="your.task" />
+        <Input name="name" id="boardName" defaultValue="your.task" autoComplete="off" />
       </div>
       <div className="grid gap-2">
         <Label htmlFor="password">Protect the board with password (leave empty if not needed)</Label>
